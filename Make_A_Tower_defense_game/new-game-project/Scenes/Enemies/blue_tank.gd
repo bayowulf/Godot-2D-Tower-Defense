4e1e155068e351c93eaf991a7592670270572414
bayowulf <jvgallagheriv@gmail.com>
2025-04-09 20:35:07 -0400

    part 9 "Shooting Turrets"
    Added code per part 9 of the tutorial series Make A Tower Defense Game by gamedevelopmentcenter

--- Make_A_Tower_defense_game/new-game-project/Scenes/Enemies/blue_tank.gd
+++ Make_A_Tower_defense_game/new-game-project/Scenes/Enemies/blue_tank.gd
@@ -2,6 +2,13 @@ extends PathFollow2D
 
 var speed = 150
 var hp = 50
+@onready var health_bar = get_node("HealthBar")
+
+func _ready():
+	health_bar.max_value = hp
+	health_bar.value = hp
+	get_node("HealthBar").set_as_top_level(true)
+
 
 func _physics_process(delta):
 	move(delta)
@@ -10,10 +17,12 @@ func _physics_process(delta):
 func move(delta):
 	#set_progress(get_progress() + speed * delta)
 	progress += speed * delta
+	health_bar.set_position(position - Vector2(30, 30))
 	#print('PROGRESS')
 	#print(progress)
 func on_hit(damage):
 	hp -= damage
+	health_bar.value = hp
 	if hp <=0:
 		on_destroy()
 		
